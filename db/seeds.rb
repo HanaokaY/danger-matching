6d74f05ff91325be929732af2623ecf77680a170
HanaokaY <okayu5757@ezweb.ne.jp>
2022-04-23 17:24:17 +0900

    show.htmlのimage_tagのclassを変更した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@ User.create!(
   name: '近所の美女',
   self_introduction: '音楽と猫と美味しいものが好き！',
   gender: 1,
-  profile_image: open("#{Rails.root}/db/dummy_images/1.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/1.jpg"),
+  image_binary: ''
 )
 User.create!(
   email: 'user2@gmail.com',
@@ -13,7 +14,8 @@ User.create!(
   name: '南区の刺客',
   self_introduction: 'クリエイターさんと話してみたい。',
   gender: 1,
-  profile_image: open("#{Rails.root}/db/dummy_images/2.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/2.jpg"),
+  image_binary: ''
 )
 User.create!(
   email: 'user3@gmail.com',
@@ -21,7 +23,8 @@ User.create!(
   name: 'ダチ姐さん',
   self_introduction: 'プロフィールをご覧いただきありがとうございます。東京でWebマーケティング関連の仕事をしています。',
   gender: 1,
-  profile_image: open("#{Rails.root}/db/dummy_images/3.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/3.jpg"),
+  image_binary: ''
 )
 User.create!(
   email: 'user4@gmail.com',
@@ -29,7 +32,8 @@ User.create!(
   name: '佐々木',
   self_introduction: '東京で美容師をしています。',
   gender: 0,
-  profile_image: open("#{Rails.root}/db/dummy_images/4.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/4.jpg"),
+  image_binary: ''
 )
 User.create!(
   email: 'user5@gmail.com',
@@ -37,7 +41,8 @@ User.create!(
   name: '東区の刺客',
   self_introduction: '普段は公認会計士として働いています',
   gender: 0,
-  profile_image: open("#{Rails.root}/db/dummy_images/5.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/5.jpg"),
+  image_binary: ''
 )
 User.create!(
   email: 'user6@gmail.com',
@@ -45,7 +50,8 @@ User.create!(
   name: 'オリバ',
   self_introduction: '週3日くらい1人ラーメンします',
   gender: 0,
-  profile_image: open("#{Rails.root}/db/dummy_images/6.jpg")
+  profile_image: open("#{Rails.root}/db/dummy_images/6.jpg"),
+  image_binary: ''
 )
 User.create!(
     email: 'user7@gmail.com',
@@ -53,5 +59,6 @@ User.create!(
     name: 'HIKI',
     self_introduction: '韓国からの刺客。1日10本タバコ吸います。',
     gender: 1,
-    profile_image: open("#{Rails.root}/db/dummy_images/7.jpg")
+    profile_image: open("#{Rails.root}/db/dummy_images/7.jpg"),
+    image_binary: ''
   )
