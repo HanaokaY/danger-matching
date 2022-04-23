abd73980fa4b70addfa56afe30fff0b77a476a8e
HanaokaY <okayu5757@ezweb.ne.jp>
2022-04-23 15:07:05 +0900

    ボタンクリックでスワイプ機能実装

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -68,9 +68,36 @@ if(location.pathname == "/users") {
                   initCards();
                 }
             });
+        });
 
-          });
+        function createButtonListener(reaction) {
+
+            let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+            if (!cards.length) return false;
+
+            let moveOutWidth = document.body.clientWidth * 2;
+
+            let card = cards[0];
+            card.classList.add('removed');
+
+            if (reaction == "like") {
+                card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+            } else {
+                card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+            }
+
+            initCards();
+
+        }
+    
+        $('#like').on('click', function() {
+          createButtonListener("like");
+        })
+    
+        $('#dislike').on('click', function() {
+          createButtonListener("dislike");
+        })
 
     });
-
 }
