a971f9ddecf2e46681f02a814a340b0064bddab8
HanaokaY <okayu5757@ezweb.ne.jp>
2022-04-23 15:16:50 +0900

    ログイン、アカウント作成後に画面遷移機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { registrations: 'registrations' }
+    controllers: {
+      registrations: 'registrations',
+      sessions: :sessions
+    }
   root 'top#index'
-  resources :users, only: [:show,:index]
+  resources :users, only: [:show, :index]
 end
